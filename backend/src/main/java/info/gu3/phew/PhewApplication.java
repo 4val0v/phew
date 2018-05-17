9988871912d720a70cc2185c81326e809268c131
K-Yosuke <bystander.k@gmail.com>
2018-05-18 01:41:18 +0900

    initialize spring boot.

---
+++ backend/src/main/java/info/gu3/phew/PhewApplication.java
@@ -0,0 +1,12 @@
+package info.gu3.phew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhewApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PhewApplication.class, args);
+  }
+}
