8b43d23000850d8645eb4759aec9b7b0905b488e
K-Yosuke <bystander.k@gmail.com>
2018-05-22 02:11:57 +0900

    add navigationBar Sample

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,20 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     NgbModule.forRoot(),
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+  ]
 })
 export class AppModule { }
