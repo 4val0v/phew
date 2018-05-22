25cabae7f3e9e49e4e1dae644ec6c90c1ab28824
K-Yosuke <bystander.k@gmail.com>
2018-05-23 00:34:04 +0900

    delelop

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TimelineComponent } from './timeline/timeline.component';
+import { SettingComponent } from './setting/setting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TimelineComponent },
+  { path: 'settings', component: SettingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material';
+import { MatToolbarModule, MatIconModule, MatButtonModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { SettingComponent } from './setting/setting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
+    TimelineComponent,
+    SettingComponent,
   ],
   imports: [
     AppRoutingModule,
@@ -20,6 +24,8 @@ import { NavigationComponent } from './navigation/navigation.component';
     NgbModule.forRoot(),
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [
